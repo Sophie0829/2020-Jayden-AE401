5c528e58c441c8629687f3f1e340603bda92edd5
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-03-21 12:36:46 +0800

    score
    及不及格

---
+++ score.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 11:28:36 2020
+
+@author: jaych
+"""
+
+
+score=input("請輸入你的成績:")
+score=int(score)
+if score>60:
+        print("恭喜,及格了!")
+else :
+        print("真可惜~不及格")
