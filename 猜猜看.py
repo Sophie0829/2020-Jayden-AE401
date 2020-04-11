d7c4db1805d8287d52f7555c02e0cb900e58c6a3
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-04-11 12:17:20 +0800

    猜猜看
    猜數字

---
+++ 猜猜看.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 11:35:55 2020
+
+@author: jaych
+"""
+
+
+#召喚模組
+import random
+num=random.randint(1,10)
+a=input("請輸入數字:")
+a=int(a)
+if a==num:
+     print("恭喜你猜對了!")
+else:
+     print("猜錯了!")
+   
+
+
+
