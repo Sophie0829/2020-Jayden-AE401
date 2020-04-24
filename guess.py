10aabea0c3a8eefaff1ed9b4890442baab74a49a
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-04-24 20:32:30 +0800

    猜數字2
    猜數字

---
+++ guess.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 20:06:31 2020
+
+@author: jaych
+"""
+import random
+num=random.randint(1,20)
+a=input("請輸入數字:")
+a=int(a)
+if a<num:
+    print("太小")
+i+1
+elif a>num:
+    print("太大")
+i+1
+elif a==num:
+    print("答對了!!!")
+else:
+    print("請繼續猜")
+i+1
+while i>=5
+    print("輸了")
