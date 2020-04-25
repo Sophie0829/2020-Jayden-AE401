4b847fd917183c6cf79256215368c7831d5a57c8
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-04-25 12:09:20 +0800

    turtle1
    烏龜畫正方形

---
+++ a.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Here you can ge
+Created on Sat Apr 25 10:53:03 2020
+
+@author: jaych
+"""
+import turtle
+
+S=turtle.Turtle()
+A=turtle.Screen()
+
+A.title("window")
+
+A.bgcolor("black")
+
+S.color("green")
+S.shape("turtle")
+
+S.forward(100)
+S.left(90)
+S.forward(100)
+S.left(90)
+S.forward(100)
+S.left(90)
+S.forward(100)
+
+turtle.done()
