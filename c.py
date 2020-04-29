cd4b5fd5fedbfd8332a81cc3fe0a974d37aad8fd
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-04-29 19:43:22 +0800

    烏龜畫圓
    烏龜畫圓

---
+++ c.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 19:21:09 2020
+
+@author: jaych
+"""
+import turtle
+S=turtle.Turtle()
+F=turtle.Screen()
+
+F.title("window")
+
+F.bgcolor("black")
+
+S.color("green")
+S.shape("turtle")
+S.pensize(10)
+
+A=0
+
+while A<36:
+    S.left(10)
+    S.forward(10)
+    A=A+1
+
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+333.
