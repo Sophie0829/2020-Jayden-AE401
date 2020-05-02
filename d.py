c5782914e0071798a3f95bfba2ef8d31d19d79db
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-05-02 12:04:23 +0800

    sport
    喜歡的運動

---
+++ d.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 11:34:26 2020
+
+@author: jaych
+"""
+
+
+sport=["游泳","羽球","跑步","棒球"]
+print(sport)
+
+sport.append("球場")
+print(sport)
+
+sport.insert(2,100)
+print(sport)
+
+sport.remove("球場")
+print(sport)
