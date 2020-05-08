34191a681357c0732d882acf6c45533fcd3131c6
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-05-08 23:02:35 +0800

    成績*5
    5個成績

---
+++ e.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 22:00:22 2020
+
+@author: jaych
+"""
+
+scores=[]
+
+while i<5:
+
+A=input("請輸入成績:")
+
+A=int(A)
+
+scores.append(A)
+
+print(scores)
+
+i=i+1
+
