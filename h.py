b5d72b034dd21ff03d5f272eabae9fdf7f2aabf6
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-05-23 12:14:31 +0800

    BIF
    BIF

---
+++ h.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 11:26:56 2020
+
+@author: jaych
+"""
+
+
+def add3(num1,num2,num3):
+    
+    total=num1+num2+num3
+    
+    return total
+
+A=add3(123456,234567,345678)
+
+print(A)
