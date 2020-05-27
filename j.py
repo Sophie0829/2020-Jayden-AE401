d5969c13706ff3aebda3f1413f4ba635810c69e8
Sophie0829 <62165741+Sophie0829@users.noreply.github.com>
2020-05-27 20:13:04 +0800

    homework

---
+++ j.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 19:35:30 2020
+
+@author: jaych
+"""
+
+
+A=[]
+B=[]
+C=[]
+
+def F(ani1,ani2,ani3):
+    
+    total=ani1+ani2+ani3
+    
+    return total
+
+if A:
+    A=F("踢","球","貓")
+    print(A)
+elif B:
+    B=F("玩","玩具","狗")
+    print(B)
+
+else C:
+    C=F("丟","球","兔子")
+    print(C)
+
+
